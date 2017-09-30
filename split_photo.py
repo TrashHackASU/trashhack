c973e40d63b2992ac74fad12ee66bebc67f57c51
daltondavis5 <daltonndavis5@gmail.com>
2017-09-30 16:36:45 -0700

    split photo now breaks photo up into pieces

--- split_photo.py
+++ split_photo.py
@@ -1,7 +1,35 @@
+from math import sqrt, ceil, floor
 from PIL import Image
 
-def get_num_pixels(filepath):
-    width, height = Image.open(open(filepath)).size
-    return (width, height)
+image = Image.open("recyclables1.jpg")
+image.show()
 
-print get_num_pixels("recyclables1.jpg")
+def slice(filename, number_tiles):
+    print "slicing"
+    image = Image.open(filename)
+
+    imageWidth, imageHeight = image.size
+    columns, rows = calculateColumnsRows(number_tiles)
+    extras = (columns * rows) - number_tiles
+    tileWidth, tileHeight = int(floor(imageWidth / columns)), int(floor(imageHeight / rows))
+
+    number = 1
+    for pos_y in range(0, imageHeight - rows, tileHeight): # -rows for rounding error.
+        for pos_x in range(0, imageWidth - columns, tileWidth): # as above.
+            area = (pos_x, pos_y, pos_x + tileWidth, pos_y + tileHeight)
+            tileImage = image.crop(area)
+
+            tileImage.show()
+            """passToModel(tileImage)"""
+
+def calculateColumnsRows(parts):
+    """
+    Calculate the number of columns and rows required to divide an image
+    into n parts.
+    Return a tuple of integers in the format (num_columns, num_rows)
+    """
+    numColumns = int(ceil(sqrt(parts)))
+    numRows = int(ceil(parts / float(numColumns)))
+    return (numColumns, numRows)
+
+slice("recyclables1.jpg", 8)
